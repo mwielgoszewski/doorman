a4fd613dd6bc79ab74d16ce170f901acc111768c
Andrew Dunham <adunham@stripe.com>
2016-05-03 16:19:50 -0700

    Allow starting a Celery worker using our test config

--- doorman/worker.py
+++ doorman/worker.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
 
 from doorman.application import create_app
 
 app = create_app()
+
+# Override config if we're testing
+if os.environ.get('DOORMAN_CELERY_TESTING'):
+    app.config.from_object('doorman.settings.TestConfig')
 
 from doorman.tasks import celery

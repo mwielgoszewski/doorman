a6d32ccbb575b1653ec9d0f265fdfee3c52572cb
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2016-05-05 23:09:04 -0400

    register error handlers, change default config to production
    begin work against #25.

--- doorman/application.py
+++ doorman/application.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 from doorman.api import blueprint as api
 from doorman.assets import assets
@@ -9,18 +9,18 @@ from doorman.views import blueprint as backend
 from doorman.extensions import (
     db, debug_toolbar, log_tee, make_celery, metrics, migrate, rule_manager
 )
-from doorman.models import Pack, Query
-from doorman.settings import DevConfig
+from doorman.settings import ProdConfig
 from doorman.tasks import celery
 from doorman.utils import get_node_health
 
 
-def create_app(config=DevConfig):
+def create_app(config=ProdConfig):
     app = Flask(__name__)
     app.config.from_object(config)
     app.config.from_envvar('DOORMAN_SETTINGS', silent=True)
 
     register_blueprints(app)
+    register_errorhandlers(app)
     register_extensions(app)
     register_filters(app)
 
@@ -41,6 +41,18 @@ def register_extensions(app):
     rule_manager.init_app(app)
     make_celery(app, celery)
     metrics.init_app(app)
+
+
+def register_errorhandlers(app):
+    """Register error handlers."""
+    def render_error(error):
+        """Render error template."""
+        # If a HTTPException, pull the `code` attribute; default to 500
+        error_code = getattr(error, 'code', 500)
+        return render_template('{0}.html'.format(error_code)), error_code
+
+    for errcode in [401, 403, 404, 500]:
+        app.errorhandler(errcode)(render_error)
 
 
 def register_filters(app):

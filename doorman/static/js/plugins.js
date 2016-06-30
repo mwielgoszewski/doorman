7800cd313d304d0701fc5735cc8a909e8cb2917e
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2016-06-30 18:10:40 -0400

    add csrf protection to ajax posts

--- doorman/static/js/plugins.js
+++ doorman/static/js/plugins.js
@@ -2,6 +2,16 @@
 
 $(function() {
 
+    var csrftoken = $('meta[name=csrf-token]').attr('content')
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken)
+            }
+        }
+    })
+
     $(function(){
         var hash = window.location.hash;
         hash && $('ul.nav a[href="' + hash + '"]').tab('show');

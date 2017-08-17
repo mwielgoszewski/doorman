d112540aa1990a6781287dba4a397922f311c115
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2017-08-16 22:10:27 -0400

    Create a scheduled task that's run with celery beat to detect when a node goes offline

--- doorman/tasks.py
+++ doorman/tasks.py
@@ -30,3 +30,74 @@ def notify_of_node_enrollment(node):
 def example_task(one, two):
     print('Adding {0} and {1}'.format(one, two))
     return one + two
+
+
+@celery.task()
+def alert_when_node_goes_offline():
+    '''
+    This task is intended to periodically comb the database to identify
+    nodes that have not posted results in some time, checked in for some
+    time, or have not posted results within some time of their last
+    checkin. The purpose of this task is to identify nodes that go offline,
+    or in some cases, nodes with corrupted osquery rocksdb databases.
+    '''
+    from collections import namedtuple
+    from itertools import imap
+    from sqlalchemy import func
+    from doorman.models import db, Node, ResultLog
+    import datetime as dt
+
+    _Node = namedtuple('Node', [
+        'id', 'host_identifier', 'node_info', 'enrolled_on', 'is_active',
+        'last_ip', 'last_checkin', 'last_result',
+    ])
+
+    query = db.session.query(
+        ResultLog.node_id,
+        Node.host_identifier,
+        Node.node_info,
+        Node.enrolled_on,
+        Node.is_active,
+        Node.last_ip,
+        Node.last_checkin,
+        func.max(ResultLog.timestamp),
+    ).join(Node).filter(Node.is_active).group_by(ResultLog.node_id, Node.id)
+
+    now = dt.datetime.utcnow()
+    calendarTime = now.strftime('%a %b %d %H:%M:%S %Y UTC')
+
+    for processed, node in enumerate(imap(_Node.make, query), 1):
+        entry = {
+            'name': 'doorman/tasks/node_offline_checks',
+            'calendarTime': calendarTime,
+            'action': 'triggered',
+        }
+        columns = entry['columns'] = {}
+
+        since_last_result = now - node.last_result
+        since_last_checkin = now - node.last_checkin
+        since_last_checkin_to_last_result = node.last_checkin - node.last_result
+
+        columns['since_last_result_seconds'] = since_last_result.total_seconds()
+        columns['since_last_checkin_seconds'] = since_last_checkin.total_seconds()
+        columns['since_last_checkin_to_last_result_seconds'] = since_last_checkin_to_last_result.total_seconds()
+
+        columns['since_last_result_days'] = since_last_result.days
+        columns['since_last_checkin_days'] = since_last_checkin.days
+        columns['since_last_checkin_to_last_result_days'] = since_last_checkin_to_last_result.days
+
+        columns['since_last_result'] = since_last_result
+        columns['since_last_checkin'] = since_last_checkin
+        columns['since_last_checkin_to_last_result'] = since_last_checkin_to_last_result
+
+        _node = dict(node._asdict())
+        _node['display_name'] = node.node_info.get('display_name') or \
+            node.node_info.get('hostname') or \
+            node.node_info.get('computer_name') or \
+            node.host_identifier
+
+        result = {'data': [entry]}
+        current_app.rule_manager.handle_log_entry(result, _node)
+
+    else:
+        return processed
